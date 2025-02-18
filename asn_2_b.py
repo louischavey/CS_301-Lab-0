98fc2abb66dbf26fc292d9c883fcb27841589d82
Louis Chavey <louis@chavey.org>
2025-02-18 15:22:06 -0600

    2/18

--- asn_2_b.py
+++ asn_2_b.py
@@ -3,7 +3,7 @@ import time
 from sonar import *
 # import Mpu6050
 import ros_robot_controller_sdk as rrc
-from map_ import *
+from map_301 import *
 
 
 print('''
@@ -57,15 +57,24 @@ oddVerts = [2, 8, 14]
 
 runTimeForwardBack = 0.15
 runTimeUpDown = 0.1
+runTimeSonar = 0.05
 
-WALKS_PER_GRID = 3
+WALKS_PER_GRID = 4 * 2
+
+TURN_ADJUST = -5
+OFFSET_ADJUST = 0
 
 ADJUST_THRESHOLD = 665 - LEG_BACK  # 665 is roughly max we can move leg before it touches other one
 
 jointIDs = list(range(1, 19))
 
+heading = 1
+
 directions = {(-1, 0): 3, (1, 0): 1, (0, -1): 4, (0, 1): 2}
 
+SCAN_SLEEP = 0.5
+WALL_THRESHOLD = 400
+sonar = Sonar()
 
 def legsUp(legID):
     for id in legID:
@@ -91,31 +100,47 @@ def legsForward(legID, adjust=0):
             if id < 10:
                 board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD - adjust]])
             else:
-                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + 5 + adjust]])
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + OFFSET_ADJUST + adjust]])
                 #board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK]])
         elif adjust < 0:
             if id < 10:
-                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD + adjust]])
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD - adjust]])
             else:
-                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + 5 - adjust]])
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + OFFSET_ADJUST + adjust]])
                 #board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK - adjust]])
 
         else:
             if id < 10:
                 board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD]])
             else:
-                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + 5]])
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + OFFSET_ADJUST]])
                 #board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK]])
 
     return
 
 
-def legsBack(legID):
+def legsBack(legID, adjust=0):
+    if adjust > ADJUST_THRESHOLD:
+        adjust = ADJUST_THRESHOLD
+    elif adjust < -ADJUST_THRESHOLD:
+        adjust = -ADJUST_THRESHOLD
     for id in legID:
-        if id < 10:
-            board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK]])
+        if adjust > 0:
+            if id < 10:
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + adjust]])
+            else:
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD + OFFSET_ADJUST - adjust]])
+        elif adjust < 0:
+            if id < 10:
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK + adjust]])
+            else:
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD + OFFSET_ADJUST - adjust]])
         else:
-            board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD]])
+            if id < 10:
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_BACK]])
+            else:
+                board.bus_servo_set_position(runTimeForwardBack, [[id, LEG_FORWARD]])
+        
     return
 
 def initRobot():
@@ -184,26 +209,26 @@ def walkBack():
     legsUp(evenVerts)
     legsBack(evenLegs)
 
-    time.sleep(1)
+    time.sleep(runTimeUpDown)
 
     legsDown(evenVerts)
 
-    time.sleep(1)
+    time.sleep(runTimeUpDown)
 
     legsForward(evenLegs)
     legsUp(oddVerts)
-    time.sleep(0.5)
+    time.sleep(runTimeUpDown)
     legsBack(oddLegs)
 
-    time.sleep(1)
+    time.sleep(runTimeUpDown)
 
     legsDown(oddVerts)
 
-    time.sleep(1)
+    time.sleep(runTimeUpDown)
 
     legsUp(evenVerts)
     legsForward(oddLegs)
-    time.sleep(0.5)
+    time.sleep(runTimeUpDown)
     return
 
 def turnRightNew(adjust=0):
@@ -217,7 +242,7 @@ def turnRightNew(adjust=0):
     time.sleep(runTimeUpDown)
 
     legsForward([1,7,10,16], adjust)
-    legsBack([13, 4])
+    legsBack([13, 4], adjust)
 
     time.sleep(runTimeUpDown)
 
@@ -229,7 +254,7 @@ def turnRightNew(adjust=0):
     #legsBack([1, 7, 4])
     #legsForward([13, 10, 16])
 
-    legsBack([1, 7, 10, 16])
+    legsBack([1, 7, 10, 16], adjust)
     legsForward([13, 4], adjust)
     time.sleep(runTimeUpDown)
     
@@ -244,7 +269,7 @@ def turnLeftNew(adjust=0):
     
     time.sleep(runTimeUpDown)
 
-    legsBack([1,7,10,16])
+    legsBack([1,7,10,16], adjust)
     legsForward([13, 4], adjust)
 
     time.sleep(runTimeUpDown)
@@ -258,7 +283,7 @@ def turnLeftNew(adjust=0):
     #legsForward([13, 10, 16])
 
     legsForward([1, 7, 10, 16], adjust)
-    legsBack([13, 4])
+    legsBack([13, 4], adjust)
 
     time.sleep(runTimeUpDown)
 
@@ -283,28 +308,30 @@ def forward(seconds):
         walkForward()
 
 def sonarRight():
-    board.bus_servo_set_position(runTimeUpDown, [[21, 110]])
-    print("sonar right")
+    board.bus_servo_set_position(runTimeSonar, [[21, 110]])
+    #print("sonar right")
     return
 
 def sonarLeft():
-    board.bus_servo_set_position(runTimeUpDown, [[21, 890]])
-    print("sonar left")
+    board.bus_servo_set_position(runTimeSonar, [[21, 890]])
+    #print("sonar left")
     return
 
 def sonarForward():
-    board.bus_servo_set_position(runTimeUpDown, [[21, 500]])
-    print("sonar fwd")
+    board.bus_servo_set_position(runTimeSonar, [[21, 500]])
+    #print("sonar fwd")
     return
 
 def gridWalk(grids):
     if grids < 0:
-        for _ in range(-grids * WALKS_PER_GRID + (grids // 6)): #Change 11.5 to be accurate
+        for _ in range(-grids * WALKS_PER_GRID - (grids // 6)): #Change 11.5 to be accurate
             walkBack()
+
     elif grids > 0:
-        for _ in range(grids * WALKS_PER_GRID): #- (grids // 6)): #Change 11.5 to be accurate
+        for _ in range(grids * WALKS_PER_GRID + (grids // 6)): #- (grids // 6)): #Change 11.5 to be accurate
             #print(grids * WALKS_PER_GRID - (grids // 6))
             walkForward()
+
 
 def getCoords():
     start = input("Enter starting location: ")
@@ -373,52 +400,58 @@ def coordMove(init, final):
 
     return
 
-heading = 1
 
-def pathStep(init, final):
+
+def pathStep(init, final, heading):
     deltaI = final[0] - init[0]
     deltaJ = final[1] - init[1]
 
     if deltaI is not 0:
-        heading = globalTurn(heading, 2)
-        gridWalk(deltaI)
+        if deltaI > 0:
+            heading = globalTurn(heading, 2)
+        elif deltaJ < 0:
+            heading = globalTurn(heading, 1)
+        gridWalk(abs(deltaI))
     if deltaJ is not 0:
-        heading = globalTurn(heading, 3)
-        gridWalk(deltaJ)   
+        if deltaJ > 0:
+            heading = globalTurn(heading, 3)
+        elif deltaJ < 0:
+            heading = globalTurn(heading, 4)
+        gridWalk(abs(deltaJ))   
 
-    return 
+    return heading
 
 
 def globalTurn(start, end):
 
-    if start is 1:
-        if end is 2:
-            turn(180)
-        elif end is 3:
-            turn(90)
-        elif end is 4: 
-            turn(-90)
-    elif start is 2:
-        if end is 1:
-            turn(180)
-        elif end is 3:
-            turn(-90)
-        elif end is 4:
-            turn(90)
-    elif start is 3:
-        if end is 1:
-            turn(-90)
-        elif end is 2:
-            turn(90)
-        elif end is 4:
-            turn(180)
-    elif start is 4:
-        if end is 1:
-            turn(90)
-        elif end is 2:
-            turn(-90)
-        elif end is 3:
-            turn(180)
+    if start == 1:
+        if end == 2:
+            turn(180, TURN_ADJUST)
+        elif end == 3:
+            turn(90, TURN_ADJUST)
+        elif end == 4: 
+            turn(-90, TURN_ADJUST)
+    elif start == 2:
+        if end == 1:
+            turn(180, TURN_ADJUST)
+        elif end == 3:
+            turn(-90, TURN_ADJUST)
+        elif end == 4:
+            turn(90, TURN_ADJUST)
+    elif start == 3:
+        if end == 1:
+            turn(-90, TURN_ADJUST)
+        elif end == 2:
+            turn(90, TURN_ADJUST)
+        elif end == 4:
+            turn(180, TURN_ADJUST)
+    elif start == 4:
+        if end == 1:
+            turn(90, TURN_ADJUST)
+        elif end == 2:
+            turn(-90, TURN_ADJUST)
+        elif end == 3:
+            turn(180, TURN_ADJUST)
 
     return end
 
@@ -496,8 +529,7 @@ def pathGen(init, final, grid):
         #print(nextCoord)
         visitList.insert(0, nextCoord)
         i = nextCoord[0]
-        j = nextCoord[1]
-    print("visitList in pathGen: ", visitList)            
+        j = nextCoord[1]           
     return visitList
 
 
@@ -505,19 +537,223 @@ def pathFollow(coordList, initHeading, finalHeading):
 
     heading = initHeading
 
-    for i in range(coordList-1):
-        pathStep(coordList[i], [coordList[i+1]])
+    for i in range(len(coordList[:-1])):
+        print(coordList[i])
+        heading = pathStep(coordList[i], coordList[i+1], heading)
+        
 
     heading = globalTurn(heading, finalHeading)
 
     return
 
+def wallDetect(grid, position, currHeading):
+
+    i = position[0]
+    j = position[1]
+    wallForward = False
+    wallLeft = False
+    wallRight = False
+
+    sonarForward()
+    time.sleep(SCAN_SLEEP)
+    print("forward sonar read in wallDetect: ", sonar.getDistance())
+    if sonar.getDistance() < WALL_THRESHOLD:
+        wallForward = True
+
+    time.sleep(SCAN_SLEEP)
+
+    sonarLeft()
+    time.sleep(SCAN_SLEEP)
+    print("left sonar read in wallDetect: ", sonar.getDistance())
+    if sonar.getDistance() < WALL_THRESHOLD:
+        wallLeft = True
+
+    time.sleep(SCAN_SLEEP * 2)
+
+    sonarRight()
+    time.sleep(SCAN_SLEEP * 2)
+    print("right sonar read in wallDetect: ", sonar.getDistance())
+    if sonar.getDistance() < WALL_THRESHOLD:
+        wallRight = True
+
+    time.sleep(SCAN_SLEEP // 2)
+
+    if currHeading == 1: # Facing North
+        if wallForward:
+            #set wall i - 1
+            grid.setObstacle(i, j, 1, 1)
+        if wallLeft:
+            #set wall j - 1
+            grid.setObstacle(i, j, 1, 4)
+        if wallRight:
+            #set wall j + 1
+            grid.setObstacle(i, j, 1, 2)
+
+    elif currHeading == 2: # Facing South
+        if wallForward:
+            #set wall i + 1
+            grid.setObstacle(i, j, 1, 3)
+        if wallLeft:
+            #set wall j + 1
+            grid.setObstacle(i, j, 1, 2)
+        if wallRight:
+            #set wall j - 1
+            grid.setObstacle(i, j, 1, 4)
+
+    elif currHeading == 3: # Facing East
+        if wallForward:
+            #set wall j + 1
+            grid.setObstacle(i, j, 1, 2)
+        if wallLeft:
+            #set wall i - 1
+            grid.setObstacle(i, j, 1, 1)
+        if wallRight:
+            #set wall i + 1
+            grid.setObstacle(i, j, 1, 3)
+
+    elif currHeading == 4: # Facing West
+        if wallForward:
+            #set wall j - 1
+            grid.setObstacle(i, j, 1, 4)
+        if wallLeft:
+            #set wall i + 1
+            grid.setObstacle(i, j, 1, 3)
+        if wallRight:
+            #set wall i - 1
+            grid.setObstacle(i, j, 1, 1)
+    return
+
+
+def mapSeek(grid, finalPosition):
+
+
+    i = 0
+    j = 0
+
+    initI = 0
+    initJ = 0
+
+    finalI = finalPosition[0]
+    finalJ = finalPosition[1]
+    finalHeading = finalPosition[2]
+
+    heading = 1
+
+    rows, cols = grid.costmap_size_row, grid.costmap_size_col
+
+    for i_ in range(rows):
+        for j_ in range(cols):
+            if grid.getCost(i_, j_) == 0:
+                grid.setCost(i_, j_, -1)
+
+    grid.setCost(initI, initJ, 0)
+
+    forks = {}
+    path = [(initI, initJ)]
+
+    wallDetect(grid, (i, j), heading)
+
+    heading = globalTurn(heading, 3)
+
+    while i != finalI or j != finalJ:
+
+        wallDetect(grid, (i,j), heading)
+        viableNeighbors = []
+
+        #Set neighbor costs
+        for key in directions.keys():
+                di = key[0]
+                dj = key[1]
+                nextI = i - di
+                nextJ = j + dj
+
+                #Check if in map bounds
+                if nextI < rows and nextI >= 0 and nextJ >= 0 and nextJ < cols:
+                    nextPosCost = grid.getCost(nextI, nextJ)
+
+                    #Check if next cell has been visited or if there is a wall
+                    if nextPosCost == -1 and grid.getNeighborObstacle(i, j, directions[key]) == 0:
+
+                        #Price next cell
+                        grid.setCost(nextI, nextJ, grid.getCost(i, j) + 1)
+
+                        viableNeighbors.append((nextI, nextJ))
+
+                        #Next cell to visit is whatever is priced last
+                        nextCoord = (nextI, nextJ)
+
+
+
+        #if all paths are blocked:
+
+        if not viableNeighbors: #This means new coord was not found
+            print("no viable neighbors")
+
+            #FIGURE OUT HOW TO TURN AROUND HERE WITH GLOBAL TURN
+            #THIS MAY NOT BE NEEDED IDK
+            #turn(180, TURN_ADJUST)
+
+            #If the path is blocked and there are no more forks, end
+            if forks == {}:
+                print("Coordinate could not be found!\n")
+                break
+
+
+            while (i, j) not in forks.keys():
+                #Follow the most recent path backwards until a fork is reached
+                heading = pathStep((i,j), path[-1], heading)
+                i, j = path[-1][0], path[-1][1]
+                path.pop()
+
+            nextI, nextJ = forks[(i,j)][-1][0], forks[(i,j)][-1][1]
+            heading = pathStep((i, j), (nextI, nextJ), heading)
+            forks[(i,j)].pop()
+
+            #If all forks are searched, remove fork
+            if len(forks[(i, j)]) == 0:
+                forks.pop((i, j))
+
+            i, j = nextI, nextJ
+
+        else:
+            #Remove next coord from neighbors
+            viableNeighbors.pop()
+            print("looking at this neighbor: ", viableNeighbors)
+
+            #if there are multiple available cells:
+            #if len(viableNeighbors) >= 1:
+            forks[(i, j)] = viableNeighbors
+
+            #Move to next cell, add in P controller to this potentially
+            heading = pathStep((i, j), nextCoord, heading)
+
+            #Update i and j
+            i, j = nextCoord[0], nextCoord[1]
+
+            path.append((i,j))
+
+        #grid.printCostMap()
+        print("intermediate obstacle map")
+        grid.printObstacleMap()
+        print("intermediate cost map")
+        grid.printCostMap()
+
+
+    globalTurn(heading, finalHeading)
+
+    return
+
 
 initRobot()
-turn(90, 10)
+#turn(90, -25)
 grid = CSME301Map()
-
+grid.clearObstacleMap()
+grid.clearCostMap()
+mapSeek(grid, [8, 8, 1]) 
+print("obstacle grid")
+grid.printObstacleMap()
 #1. Be able to accept a starting position xs and end position xg
+'''
 coords = getCoords()
 
 #2. Set the cost of each grid cell
@@ -529,16 +765,18 @@ print(steps)
 
 #4. Generate command sequence... call the commands developed in Step 1
 #5. Walk the path. 
-#pathFollow(steps, coords[0][2], coords[1][2])
+grid.printCostMap()
+grid.printObstacleMap()
 
+pathFollow(steps, coords[0][2], coords[1][2])
+'''
 
 #initRobot()
 
 #coords = getCoords()
 
 #coordMove(coords[0], coords[1])
-grid.printCostMap()
-grid.printObstacleMap()
+
 
 #coords = getCoords()
 
